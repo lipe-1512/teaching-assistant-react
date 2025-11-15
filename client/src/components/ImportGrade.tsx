333236f638b4d7ce8e5bc8fd4fd695bf5594dc97
pauloborba <phmb@cin.ufpe.br>
2025-11-15 18:12:51 -0300

    fixes typo

--- client/src/components/ImportGrade.tsx
+++ client/src/components/ImportGrade.tsx
@@ -6,5 +6,5 @@ interface ImportGradeComponentProps {
 export const ImportGradeComponent: React.FC<ImportGradeComponentProps> = (
   { classID = "" }
 ) => { 
-  return (<h1>Importcação de Notas de planilhas</h1>)
+  return (<h1>Importação de Notas</h1>)
 };
